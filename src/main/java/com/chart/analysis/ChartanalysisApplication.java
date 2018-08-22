c0f7e5f41779d68a63e7ef0b7c994bad2cb0a79e
SagarOnline <chopade.sagar@gmail.com>
2018-08-22 12:26:34 +0530

    initial checkin

---
+++ src/main/java/com/chart/analysis/ChartanalysisApplication.java
@@ -0,0 +1,12 @@
+package com.chart.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartanalysisApplication.class, args);
+	}
+}
